7506635ef15ccce086db7d36672e2985d721cfab
Eric Schulze <eric.schulze001@gmail.com>
2020-05-17 21:26:44 -0400

    initial commit

---
+++ Escape Room Tool/Escape Room Tool/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Escape Room Tool
+//
+//  Created by Eric Schulze on 5/15/20.
+//  Copyright © 2020 WE Develop. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
